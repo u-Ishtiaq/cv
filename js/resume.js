2509ce6d9af9e1af699717c9f13cdc26c2f653ef
u-Ishtiaq <32953252+u-Ishtiaq@users.noreply.github.com>
2020-03-12 18:54:11 +0000

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -9,7 +9,7 @@
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
+        }, 10, "easeInOutExpo");
         return false;
       }
     }
