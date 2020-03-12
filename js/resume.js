c3d174a5f40480ee95780fe0e1bee6571d219ffd
u-Ishtiaq <32953252+u-Ishtiaq@users.noreply.github.com>
2020-03-12 18:56:41 +0000

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,6 @@
 (function($) {
   "use strict"; // Start of use strict
-
+ 
   // Smooth scrolling using jQuery easing if want undo slashes
   //$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     //if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
