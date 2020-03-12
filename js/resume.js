53eb85ceea35e7e144f82fdcf37b12c89638d5b7
u-Ishtiaq <32953252+u-Ishtiaq@users.noreply.github.com>
2020-03-12 19:08:48 +0000

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -2,18 +2,18 @@
   "use strict"; // Start of use strict
  
   // Smooth scrolling using jQuery easing if want undo slashes
-  //$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="-"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      //target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      //if (target.length) {
-       // $('html, body').animate({
-       //   scrollTop: (target.offset().top)
-       // }, 1000, "easeInOutExpo");
-        //return false;
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
      }
-    //} 
- //});
+    } 
+ });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
